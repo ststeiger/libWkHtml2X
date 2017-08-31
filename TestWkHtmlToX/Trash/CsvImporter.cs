1cc053b329db975da61269edb54e0b6c48f9d14d
Me <me@localhost.com>
2017-08-31 19:39:28 +0200

    CsvParser removed namespace imports

--- TestWkHtmlToX/Trash/CsvImporter.cs
+++ TestWkHtmlToX/Trash/CsvImporter.cs
@@ -29,121 +29,29 @@ namespace TestWkHtmlToX.Trash
 
         public static void TestImport()
         {
-
-            string fn = @"C:\Users\anonymous\Downloads\CH\CH_semicolon.csv";
-
-            System.Collections.Generic.List<System.Collections.Generic.List<string>> dl = Trash.CsvParser.Parse(fn);
-            System.Console.WriteLine(dl.Count);
-
-
-            System.Data.DataTable dt = new System.Data.DataTable();
-
-            using (System.Data.Common.DbDataAdapter da = new System.Data.SqlClient.SqlDataAdapter("SELECT * FROM geoname WHERE (1=2)", gc()))
+            string fileName = @"C:\Users\anonymous\Downloads\CH\CH_semicolon.csv";
+            using (System.Data.DataTable dt = new System.Data.DataTable())
             {
-                System.Data.Common.DbCommandBuilder cb = new System.Data.SqlClient.SqlCommandBuilder();
-                cb.DataAdapter = da;
-                da.DeleteCommand = cb.GetDeleteCommand();
-                da.InsertCommand = cb.GetInsertCommand();
-                da.UpdateCommand = cb.GetUpdateCommand();
 
-
-
-                da.Fill(dt);
-
-                System.Console.WriteLine(dt.Columns.Count);
-
-
-                /*
-                foreach (System.Collections.Generic.List<string> lsColumns in dl)
+                using (System.Data.Common.DbDataAdapter da = new System.Data.SqlClient.SqlDataAdapter("SELECT * FROM geoname WHERE (1=2)", gc()))
                 {
-                    System.Data.DataRow dr = dt.NewRow();
+                    System.Data.Common.DbCommandBuilder cb = new System.Data.SqlClient.SqlCommandBuilder();
+                    cb.DataAdapter = da;
+                    da.DeleteCommand = cb.GetDeleteCommand();
+                    da.InsertCommand = cb.GetInsertCommand();
+                    da.UpdateCommand = cb.GetUpdateCommand();
 
-                    for (int j = 0; j < lsColumns.Count; ++j)
-                    {
-                        string item = lsColumns[j];
-                        // System.Console.WriteLine(item);
+                    da.Fill(dt);
+                    System.Console.WriteLine(dt.Columns.Count);
 
-                        // if (j >= dt.Columns.Count) continue;
+                    CsvParser.ParseTable(fileName, dt);
 
-                        System.Type tt = dt.Columns[j].DataType;
+                    da.Update(dt);
+                } // End Using da
 
-                        if (object.ReferenceEquals(tt, typeof(string)))
-                        {
-                            dr[j] = item;
-                        }
-                        else if (object.ReferenceEquals(tt, typeof(int)))
-                        {
-                            int a = -1;
-                            int.TryParse(item, out a);
-                            dr[j] = a;
-                        }
-                        else if (object.ReferenceEquals(tt, typeof(decimal)))
-                        {
-                            decimal a = -1;
-                            decimal.TryParse(item, out a);
-                            dr[j] = a;
-                        }
-                        else if (object.ReferenceEquals(tt, typeof(System.DateTime)))
-                        {
-                            System.DateTime a = System.DateTime.MinValue;
-                            System.DateTime.TryParse(item, out a);
-                            dr[j] = a;
-                        }
-                        else
-                            System.Console.WriteLine(tt);
-                    } // Next j 
+            } // End Using dt
 
-                    dt.Rows.Add(dr);
-                }
-                */
-
-
-                for (int i = 0; i < dl.Count; ++i)
-                {
-                    System.Data.DataRow dr = dt.NewRow();
-
-                    for (int j = 0; j < dl[i].Count; ++j)
-                    {
-                        string item = dl[i][j];
-
-                        // System.Console.WriteLine(item);
-                        // if (j >= dt.Columns.Count) continue;
-
-                        System.Type tt = dt.Columns[j].DataType;
-
-                        if (object.ReferenceEquals(tt, typeof(string)))
-                        {
-                            dr[j] = item;
-                        }
-                        else if (object.ReferenceEquals(tt, typeof(int)))
-                        {
-                            int a = -1;
-                            int.TryParse(item, out a);
-                            dr[j] = a;
-                        }
-                        else if (object.ReferenceEquals(tt, typeof(decimal)))
-                        {
-                            decimal a = -1;
-                            decimal.TryParse(item, out a);
-                            dr[j] = a;
-                        }
-                        else if (object.ReferenceEquals(tt, typeof(System.DateTime)))
-                        {
-                            System.DateTime a = System.DateTime.MinValue;
-                            System.DateTime.TryParse(item, out a);
-                            dr[j] = a;
-                        }
-                        else
-                            System.Console.WriteLine(tt);
-                    } // Next j 
-
-                    dt.Rows.Add(dr);
-                } // Next i 
-
-                da.Update(dt);
-            } // End Using da 
-
-        }
+        } // End Sub TestImport 
 
 
     }
--- TestWkHtmlToX/Trash/CsvParser.cs
+++ TestWkHtmlToX/Trash/CsvParser.cs
@@ -1,7 +1,4 @@
 ﻿
-using System.Collections.Generic;
-
-
 namespace TestWkHtmlToX.Trash
 {
 
@@ -9,9 +6,145 @@ namespace TestWkHtmlToX.Trash
     public static class CsvParser
     {
 
+
         public static System.Collections.Generic.List<System.Collections.Generic.List<string>> Parse(string path)
         {
             return Parse(path, ';', '"');
+        }
+
+
+        public static System.Data.DataTable ParseTable(string path)
+        {
+            return ParseTable(path, null, ';', '"');
+        }
+
+
+        public static System.Data.DataTable ParseTable(string path, System.Data.DataTable dt)
+        {
+            return ParseTable(path, dt, ';', '"');
+        }
+
+
+        public static System.Data.DataTable ParseTable(string path, System.Data.DataTable dt, char delimiter, char qualifier)
+        {
+            System.Collections.Generic.List<System.Collections.Generic.List<string>> dl = Parse(path, delimiter, qualifier);
+            int colCount = dt.Columns.Count;
+
+            if (dt == null)
+            {
+                dt = new System.Data.DataTable();
+                for (int i = 0; i < colCount; ++i)
+                {
+                    dt.Columns.Add("Column_" + (i + 1).ToString(), typeof(string));
+                } // Next i 
+
+            } // End if (dt == null) 
+
+            colCount--;
+
+            for (int i = 0; i < dl.Count; ++i)
+            {
+                System.Data.DataRow dr = dt.NewRow();
+
+                for (int j = 0; j < dl[i].Count; ++j)
+                {
+                    if (j > colCount) continue;
+
+                    string item = dl[i][j];
+                    // System.Console.WriteLine(item);
+                    
+                    System.Type columnType = dt.Columns[j].DataType;
+
+                    if (object.ReferenceEquals(columnType, typeof(string)))
+                    {
+                        dr[j] = item;
+                    }
+                    else if (object.ReferenceEquals(columnType, typeof(bool)))
+                    {
+                        bool a = false;
+                        if (!bool.TryParse(item, out a))
+                            throw new System.IO.InvalidDataException("Could not cast \"" + item + "\" to bool.");
+
+                        dr[j] = a;
+                    }
+                    else if (object.ReferenceEquals(columnType, typeof(char)))
+                    {
+                        char a = '\0';
+                        if (!char.TryParse(item, out a))
+                            throw new System.IO.InvalidDataException("Could not cast \"" + item + "\" to char.");
+
+                        dr[j] = a;
+                    }
+                    else if (object.ReferenceEquals(columnType, typeof(int)))
+                    {
+                        int a = 0;
+                        if (!int.TryParse(item, out a))
+                            throw new System.IO.InvalidDataException("Could not cast \"" + item + "\" to int.");
+
+                        dr[j] = a;
+                    }
+                    else if (object.ReferenceEquals(columnType, typeof(uint)))
+                    {
+                        uint a = 0;
+                        if (!uint.TryParse(item, out a))
+                            throw new System.IO.InvalidDataException("Could not cast \"" + item + "\" to uint.");
+
+                        dr[j] = a;
+                    }
+                    else if (object.ReferenceEquals(columnType, typeof(long)))
+                    {
+                        long a = 0;
+                        if (!long.TryParse(item, out a))
+                            throw new System.IO.InvalidDataException("Could not cast \"" + item + "\" to long.");
+
+                        dr[j] = a;
+                    }
+                    else if (object.ReferenceEquals(columnType, typeof(ulong)))
+                    {
+                        ulong a = 0;
+                        if (!ulong.TryParse(item, out a))
+                            throw new System.IO.InvalidDataException("Could not cast \"" + item + "\" to ulong.");
+
+                        dr[j] = a;
+                    }
+                    else if (object.ReferenceEquals(columnType, typeof(float)))
+                    {
+                        float a = 0;
+                        if (!float.TryParse(item, out a))
+                            throw new System.IO.InvalidDataException("Could not cast \"" + item + "\" to float.");
+
+                        dr[j] = a;
+                    }
+                    else if (object.ReferenceEquals(columnType, typeof(double)))
+                    {
+                        double a = 0;
+                        if (!double.TryParse(item, out a))
+                            throw new System.IO.InvalidDataException("Could not cast \"" + item + "\" to float.");
+
+                        dr[j] = a;
+                    }
+                    else if (object.ReferenceEquals(columnType, typeof(decimal)))
+                    {
+                        decimal a = 0;
+                        if (!decimal.TryParse(item, out a))
+                            throw new System.IO.InvalidDataException("Could not cast \"" + item + "\" to decimal.");
+                        dr[j] = a;
+                    }
+                    else if (object.ReferenceEquals(columnType, typeof(System.DateTime)))
+                    {
+                        System.DateTime a = System.DateTime.MinValue;
+                        if (!System.DateTime.TryParse(item, out a))
+                            throw new System.IO.InvalidDataException("Could not cast \"" + item + "\" to System.DateTime.");
+                        dr[j] = a;
+                    }
+                    else
+                        throw new System.NotImplementedException("DataType \"" + columnType.FullName + "\" has not been implemented, yet.");
+                } // Next j 
+
+                dt.Rows.Add(dr);
+            } // Next i 
+
+            return dt;
         }
 
 
@@ -32,17 +165,21 @@ namespace TestWkHtmlToX.Trash
         }
 
 
-        public static List<List<string>> Parse(System.IO.TextReader reader, char delimiter, char qualifier)
+        public static System.Collections.Generic.List<System.Collections.Generic.List<string>> 
+            Parse(System.IO.TextReader reader, char delimiter, char qualifier)
         {
             return Parse(reader, 0, delimiter, qualifier);
         }
 
-        public static List<List<string>> Parse(System.IO.TextReader reader, int maxNum, char delimiter, char qualifier)
+
+        public static System.Collections.Generic.List<System.Collections.Generic.List<string>> 
+            Parse(System.IO.TextReader reader, int maxNum, char delimiter, char qualifier)
         {
-            List<List<string>> records = new List<List<string>>();
+            System.Collections.Generic.List<System.Collections.Generic.List<string>> records = 
+                new System.Collections.Generic.List<System.Collections.Generic.List<string>>();
 
             bool inQuote = false;
-            List<string> record = new List<string>();
+            System.Collections.Generic.List<string> record = new System.Collections.Generic.List<string>();
             System.Text.StringBuilder sb = new System.Text.StringBuilder();
 
             while (reader.Peek() != -1)
@@ -59,6 +196,7 @@ namespace TestWkHtmlToX.Trash
                     {
                         if (readChar == '\r')
                             sb.Append('\r');
+
                         sb.Append('\n');
                     }
                     else
@@ -68,7 +206,7 @@ namespace TestWkHtmlToX.Trash
                             record.Add(sb.ToString());
                             //sb.Clear();
                             sb.Length = 0;
-                        }
+                        } // End if (record.Count > 0 || sb.Length > 0)
 
                         if (record.Count > 0)
                         {
@@ -78,12 +216,11 @@ namespace TestWkHtmlToX.Trash
                             // return if maxNum is reached - fetch all if maxNum = 0
                             if (maxNum == 0 && records.Count == maxNum)
                                 return records;
-                        }
+                        } // End if (record.Count > 0)
 
-
-                        record = new List<string>(record.Count);
-                    }
-                }
+                        record = new System.Collections.Generic.List<string>(record.Count);
+                    } // End else of if (inQuote)
+                } // End if (readChar == '\n' || (readChar == '\r' && (char)reader.Peek() == '\n'))
                 else if (sb.Length == 0 && !inQuote)
                 {
                     if (readChar == qualifier)
@@ -100,7 +237,7 @@ namespace TestWkHtmlToX.Trash
                     }
                     else
                         sb.Append(readChar);
-                }
+                } // End else if (sb.Length == 0 && !inQuote)
                 else if (readChar == delimiter)
                 {
                     if (inQuote)
@@ -111,7 +248,7 @@ namespace TestWkHtmlToX.Trash
                         // sb.Clear();
                         sb.Length = 0;
                     }
-                }
+                } // End else if (readChar == delimiter) 
                 else if (readChar == qualifier)
                 {
                     if (inQuote)
@@ -126,7 +263,7 @@ namespace TestWkHtmlToX.Trash
                     }
                     else
                         sb.Append(readChar);
-                }
+                } // End else if (readChar == qualifier) 
                 else
                     sb.Append(readChar);
             } // Whend 
@@ -139,6 +276,10 @@ namespace TestWkHtmlToX.Trash
                 records.Add(record);
 
             return records;
-        }
-    }
-}
+        } // End Function Parse 
+
+
+    } // End Class CsvParser
+
+
+} // End Namespace TestWkHtmlToX.Trash 
