5749ecf35905b9d6713996efc1943411da281336
ststeiger <quandary82@hailmail.net>
2017-09-20 11:12:04 +0200

    Fix object-disposal

---
+++ libWkHtmlToX/ProcessManager.cs
@@ -0,0 +1,227 @@
+﻿
+namespace libWkHtml2X
+{
+
+
+    public class ProcessManager : System.IDisposable
+    {
+
+        public delegate void OutputCallback_t(string strText);
+        public OutputCallback_t OutputCallback;
+
+        private string m_exePath;
+        private string m_arguments;
+
+        private System.Diagnostics.Process m_currentProcess;
+        private System.IO.StreamWriter m_inputStream;
+        private System.IO.BinaryReader m_outputReader;
+
+
+        public ProcessManager(string html, string executable, string args)
+            : this(html, executable, args, new OutputCallback_t(OnOutputDefault))
+        { } // End Constructor
+
+
+        public ProcessManager(string html, string executable, string args, OutputCallback_t outputCallback)
+        {
+            this.m_exePath = executable;
+            this.m_arguments = args;
+
+            OutputCallback = outputCallback;
+        } // End Constructor
+
+
+        private static void OnOutputDefault(string strText)
+        {
+            // this.txtOutput.AppendText(strText);
+            System.Console.WriteLine(strText);
+        } // End Sub AddTextToOutputTextBox
+
+
+        //private void Close()
+        //{
+        //    this.m_inputStream.WriteLine("Closing program");
+        //    this.m_inputStream.Close();
+
+        //    // this.m_currentProcess.WaitForExit();
+        //    // this.m_currentProcess.Kill()
+
+        //    this.m_currentProcess.Close();
+        //    this.m_currentProcess.Dispose();
+        //} // End Sub btnQuit_Click
+
+
+        private void ConsoleOutputHandler(object sendingProcess, System.Diagnostics.DataReceivedEventArgs outLine)
+        {
+            if (!string.IsNullOrEmpty(outLine.Data))
+            {
+                OutputCallback(System.Environment.NewLine + outLine.Data);
+            } // End if (!String.IsNullOrEmpty(outLine.Data))
+
+        } // End Sub ConsoleOutputHandler
+
+
+        public void WriteStandardInput(string command)
+        {
+            if (this.m_currentProcess.HasExited)
+            {
+                throw new System.InvalidOperationException("The process has exited...");
+            } // End if (this.m_currentProcess.HasExited)
+
+            this.m_inputStream.WriteLine(command);
+            this.m_inputStream.Flush();
+            // this.m_inputStream.Close();
+        } // End Sub Execute
+
+
+        public int ReadOutputStream(System.IO.Stream targetStream)
+        {
+            byte[] buffer = new byte[65536];
+            int size = 0;
+            int bytesRead;
+            //while ((bytesRead = this.m_currentProcess.StandardOutput.BaseStream.Read(buffer, 0, buffer.Length)) > 0)
+            //{
+            //    targetStream.Write(buffer, 0, bytesRead);
+            //    size += bytesRead;
+            //} // Whend 
+
+            //using (System.IO.BinaryReader reader = new System.IO.BinaryReader(this.m_currentProcess.StandardOutput.BaseStream))
+            //{
+            //while ((bytesRead = reader.Read(buffer, 0, buffer.Length)) > 0)
+            //{
+            //    targetStream.Write(buffer, 0, bytesRead);
+            //    size += bytesRead;
+            //} // Whend 
+            //}
+
+            while ((bytesRead = this.m_outputReader.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                targetStream.Write(buffer, 0, bytesRead);
+                size += bytesRead;
+            } // Whend 
+
+            return size;
+        }
+
+
+        public byte[] ReadOutputStream()
+        {
+            byte[] outputBytes = null;
+
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+            {
+                int size = this.ReadOutputStream(ms);
+                outputBytes = ms.ToArray();
+            }
+
+            return outputBytes;
+        }
+
+
+        public void ProcessExited(object sender, System.EventArgs e)
+        {
+            System.Console.WriteLine("Process stopped.");
+        } // End Sub ProcessExited
+
+        public bool WaitForExit(int milliseconds)
+        {
+            return this.m_currentProcess.WaitForExit(milliseconds);
+        } // End Sub ProcessExited
+
+
+        public void Start()
+        {
+            this.m_currentProcess = new System.Diagnostics.Process();
+
+            //if (System.Environment.OSVersion.Platform == System.PlatformID.Unix)
+            //    this.m_currentProcess.StartInfo.FileName = "/bin/bash";
+            //else
+            //    this.m_currentProcess.StartInfo.FileName = "cmd.exe";
+
+            this.m_currentProcess.StartInfo.FileName = this.m_exePath;
+            this.m_currentProcess.StartInfo.Arguments = this.m_arguments;
+
+
+            this.m_currentProcess.StartInfo.UseShellExecute = false;
+            this.m_currentProcess.StartInfo.CreateNoWindow = true;
+            this.m_currentProcess.StartInfo.RedirectStandardInput = true;
+            this.m_currentProcess.StartInfo.RedirectStandardOutput = true;
+            this.m_currentProcess.StartInfo.RedirectStandardError = true;
+
+            this.m_currentProcess.EnableRaisingEvents = true;
+            this.m_currentProcess.Exited += new System.EventHandler(ProcessExited);
+            this.m_currentProcess.ErrorDataReceived += new System.Diagnostics.DataReceivedEventHandler(ConsoleOutputHandler);
+            //this.m_currentProcess.OutputDataReceived += new System.Diagnostics.DataReceivedEventHandler(ConsoleOutputHandler);
+
+            this.m_currentProcess.Start();
+
+
+            // this.m_inputStream = this.m_currentProcess.StandardInput;
+            // https://stackoverflow.com/questions/2855675/process-standardinput-encoding-problem
+            this.m_inputStream = new System.IO.StreamWriter(this.m_currentProcess.StandardInput.BaseStream, System.Text.Encoding.UTF8);
+            // new System.IO.StreamReader(this.m_currentProcess.StandardOutput.BaseStream, System.Text.Encoding.UTF8);
+            // new System.IO.BinaryReader(this.m_currentProcess.StandardOutput.BaseStream);
+            this.m_outputReader = new System.IO.BinaryReader(this.m_currentProcess.StandardOutput.BaseStream);
+
+            // this.m_currentProcess.BeginOutputReadLine();
+            this.m_currentProcess.BeginErrorReadLine();
+        } // End Sub Init
+
+
+
+
+        private bool disposedValue = false; // Dient zur Erkennung redundanter Aufrufe.
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: verwalteten Zustand (verwaltete Objekte) entsorgen.
+                    // if(this.m_currentProcess != null) this.m_currentProcess.Close();
+
+                    if (this.m_inputStream != null)
+                        this.m_inputStream.Dispose();
+
+                    if (this.m_outputReader != null)
+                    { 
+#if NET_2_0
+                        this.m_outputReader.Close();
+#else
+                        this.m_outputReader.Dispose();
+#endif
+                    }
+
+
+                    if (this.m_currentProcess != null)
+                        this.m_currentProcess.Dispose();
+                }
+
+                // TODO: nicht verwaltete Ressourcen (nicht verwaltete Objekte) freigeben und Finalizer weiter unten überschreiben.
+                // TODO: große Felder auf Null setzen.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: Finalizer nur überschreiben, wenn Dispose(bool disposing) weiter oben Code für die Freigabe nicht verwalteter Ressourcen enthält.
+        // ~TestProcess() {
+        //   // Ändern Sie diesen Code nicht. Fügen Sie Bereinigungscode in Dispose(bool disposing) weiter oben ein.
+        //   Dispose(false);
+        // }
+
+        // Dieser Code wird hinzugefügt, um das Dispose-Muster richtig zu implementieren.
+        void System.IDisposable.Dispose()
+        {
+            // Ändern Sie diesen Code nicht. Fügen Sie Bereinigungscode in Dispose(bool disposing) weiter oben ein.
+            Dispose(true);
+            // TODO: Auskommentierung der folgenden Zeile aufheben, wenn der Finalizer weiter oben überschrieben wird.
+            // GC.SuppressFinalize(this);
+        }
+
+
+    } // End Class TestProcess
+
+
+} // End Namespace TestWkHtmlToX
--- libWkHtmlToX/ProcessTesting.cs
+++ libWkHtmlToX/ProcessTesting.cs
@@ -14,7 +14,7 @@ namespace libWkHtml2X
             string wkPdf = System.IO.Path.Combine(f, "wkhtmltopdf.exe");
             string wkImg = System.IO.Path.Combine(f, "wkhtmltoimage.exe");
 
-            
+
             // string file = VisualStudioHelper.MapSolutionPath(@"~/TestFiles/simplePage.htm");
             // string html = System.IO.File.ReadAllText(file, System.Text.Encoding.UTF8);
 
@@ -37,226 +37,22 @@ Hello world: 안녕하세요...
 
             byte[] pdfBytes = null;
 
-            using (TestProcess p = new TestProcess(html, wkPdf, args))
+            using (ProcessManager p = new ProcessManager(html, wkPdf, args))
             {
                 p.Start();
                 p.WriteStandardInput(html);
                 pdfBytes = p.ReadOutputStream();
 
                 System.Console.WriteLine("waiting");
-                bool b = p.WaitForExit(5000);                
+                bool b = p.WaitForExit(5000);
                 System.Console.WriteLine(b);
             } // End Using p 
+
             System.IO.File.WriteAllBytes(@"d:\test\pdfBytes.pdf", pdfBytes);
-
-        }
-    }
+        } // End Sub Test
 
 
-    public class TestProcess : System.IDisposable
-    {
-        private System.Diagnostics.Process CurrentProcess;
-        private System.IO.StreamWriter swInputStream;
-        private System.IO.BinaryReader swOutputReader;
-
-        private delegate void OutputCallback_t(string strText);
-        private OutputCallback_t OutputCallback;
-        
-        private string m_ExePath;
-        private string m_Arguments;
+    } // End Class ProcessTesting 
 
 
-        public TestProcess(string html, string executable, string args)
-        {
-            this.m_ExePath = executable;
-            this.m_Arguments = args;
-
-            OutputCallback = new OutputCallback_t(OnOutput);
-        } // End Constructor
-
-
-        public void OnOutput(string strText)
-        {
-            // this.txtOutput.AppendText(strText);
-            System.Console.WriteLine(strText);
-        } // End Sub AddTextToOutputTextBox
-
-
-        //private void Close()
-        //{
-        //    swInputStream.WriteLine("Closing program");
-        //    swInputStream.Close();
-
-        //    // this.CurrentProcess.WaitForExit();
-        //    // this.CurrentProcess.Kill()
-
-        //    this.CurrentProcess.Close();
-        //    this.CurrentProcess.Dispose();
-        //} // End Sub btnQuit_Click
-
-
-        private void ConsoleOutputHandler(object sendingProcess, System.Diagnostics.DataReceivedEventArgs outLine)
-        {
-            if (!string.IsNullOrEmpty(outLine.Data))
-            {
-                OutputCallback(System.Environment.NewLine + outLine.Data);
-            } // End if (!String.IsNullOrEmpty(outLine.Data))
-
-        } // End Sub ConsoleOutputHandler
-
-
-        public void WriteStandardInput(string command)
-        {
-            if (this.CurrentProcess.HasExited)
-            {
-                throw new System.InvalidOperationException("The process has exited...");
-            } // End if (this.CurrentProcess.HasExited)
-
-            swInputStream.WriteLine(command);
-            swInputStream.Flush();
-            // swInputStream.Close();
-        } // End Sub Execute
-
-
-        public int ReadOutputStream(System.IO.Stream targetStream)
-        {
-            byte[] buffer = new byte[65536];
-            int size = 0;
-            int bytesRead;
-            //while ((bytesRead = this.CurrentProcess.StandardOutput.BaseStream.Read(buffer, 0, buffer.Length)) > 0)
-            //{
-            //    targetStream.Write(buffer, 0, bytesRead);
-            //    size += bytesRead;
-            //} // Whend 
-
-            //using (System.IO.BinaryReader reader = new System.IO.BinaryReader(this.CurrentProcess.StandardOutput.BaseStream))
-            //{
-            //while ((bytesRead = reader.Read(buffer, 0, buffer.Length)) > 0)
-            //{
-            //    targetStream.Write(buffer, 0, bytesRead);
-            //    size += bytesRead;
-            //} // Whend 
-            //}
-
-            while ((bytesRead = this.swOutputReader.Read(buffer, 0, buffer.Length)) > 0)
-            {
-                targetStream.Write(buffer, 0, bytesRead);
-                size += bytesRead;
-            } // Whend 
-
-            return size;
-        }
-
-
-        public byte[] ReadOutputStream()
-        {
-            byte[] outputBytes = null;
-
-            using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
-            {
-                int size = this.ReadOutputStream(ms);
-                outputBytes = ms.ToArray();
-            }
-
-            return outputBytes;
-        }
-
-
-        public void ProcessExited(object sender, System.EventArgs e)
-        {
-            System.Console.WriteLine("Process stopped.");
-        } // End Sub ProcessExited
-
-        public bool WaitForExit(int milliseconds)
-        {
-            return this.CurrentProcess.WaitForExit(milliseconds);
-        } // End Sub ProcessExited
-
-
-        public void Start()
-        {
-            this.CurrentProcess = new System.Diagnostics.Process();
-
-            //if (System.Environment.OSVersion.Platform == System.PlatformID.Unix)
-            //    this.CurrentProcess.StartInfo.FileName = "/bin/bash";
-            //else
-            //    this.CurrentProcess.StartInfo.FileName = "cmd.exe";
-
-            this.CurrentProcess.StartInfo.FileName = this.m_ExePath;
-            this.CurrentProcess.StartInfo.Arguments = this.m_Arguments;
-
-
-            this.CurrentProcess.StartInfo.UseShellExecute = false;
-            this.CurrentProcess.StartInfo.CreateNoWindow = true;
-            this.CurrentProcess.StartInfo.RedirectStandardInput = true;
-            this.CurrentProcess.StartInfo.RedirectStandardOutput = true;
-            this.CurrentProcess.StartInfo.RedirectStandardError = true;
-
-            this.CurrentProcess.EnableRaisingEvents = true;
-            this.CurrentProcess.Exited += new System.EventHandler(ProcessExited);
-            this.CurrentProcess.ErrorDataReceived += new System.Diagnostics.DataReceivedEventHandler(ConsoleOutputHandler);
-            //this.CurrentProcess.OutputDataReceived += new System.Diagnostics.DataReceivedEventHandler(ConsoleOutputHandler);
-
-            this.CurrentProcess.Start();
-            
-            // swInputStream = this.CurrentProcess.StandardInput;
-            // https://stackoverflow.com/questions/2855675/process-standardinput-encoding-problem
-            this.swInputStream = new System.IO.StreamWriter(this.CurrentProcess.StandardInput.BaseStream, System.Text.Encoding.UTF8);
-            // new System.IO.StreamReader(this.CurrentProcess.StandardOutput.BaseStream, System.Text.Encoding.UTF8);
-            // new System.IO.BinaryReader(this.CurrentProcess.StandardOutput.BaseStream);
-            this.swOutputReader = new System.IO.BinaryReader(this.CurrentProcess.StandardOutput.BaseStream);
-
-            // this.CurrentProcess.BeginOutputReadLine();
-            this.CurrentProcess.BeginErrorReadLine();
-        } // End Sub Init
-
-
-
-
-        private bool disposedValue = false; // Dient zur Erkennung redundanter Aufrufe.
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: verwalteten Zustand (verwaltete Objekte) entsorgen.
-                    // if(this.CurrentProcess != null) this.CurrentProcess.Close();
-
-                    if (this.swInputStream != null)
-                        this.swInputStream.Dispose();
-
-                    // if (this.swOutputReader != null) this.swOutputReader.Close();
-
-                    if (this.CurrentProcess != null)
-                        this.CurrentProcess.Dispose();
-                }
-
-                // TODO: nicht verwaltete Ressourcen (nicht verwaltete Objekte) freigeben und Finalizer weiter unten überschreiben.
-                // TODO: große Felder auf Null setzen.
-
-                disposedValue = true;
-            }
-        }
-
-        // TODO: Finalizer nur überschreiben, wenn Dispose(bool disposing) weiter oben Code für die Freigabe nicht verwalteter Ressourcen enthält.
-        // ~TestProcess() {
-        //   // Ändern Sie diesen Code nicht. Fügen Sie Bereinigungscode in Dispose(bool disposing) weiter oben ein.
-        //   Dispose(false);
-        // }
-
-        // Dieser Code wird hinzugefügt, um das Dispose-Muster richtig zu implementieren.
-        void System.IDisposable.Dispose()
-        {
-            // Ändern Sie diesen Code nicht. Fügen Sie Bereinigungscode in Dispose(bool disposing) weiter oben ein.
-            Dispose(true);
-            // TODO: Auskommentierung der folgenden Zeile aufheben, wenn der Finalizer weiter oben überschrieben wird.
-            // GC.SuppressFinalize(this);
-        }
-
-
-    } // End Class TestProcess
-
-
-} // End Namespace TestWkHtmlToX
+} // End Namespace libWkHtml2X 
